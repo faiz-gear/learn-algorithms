f62ec9c33af5148c05e0158ccb26cb0209545969
faiz-gear <1824056030@qq.com>
2024-04-08 17:55:21 +0800

    只出现一次的数字

---
+++ 7-practice/08-leetcode-136.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  return nums.reduce((prev, cur) => prev ^ cur, 0)
+}
+// @lc code=end
